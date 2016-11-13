fab1c71530dd666710964d483d90efc8c5abb76e
schuhbacca <schuh.m.ryan@gmail.com>
2016-11-12 22:06:56 -0500

    Updated project to have process control the RC car. Added arduino code.

---
+++ src/main/java/com/schuhr/Comm/Arduino.java
@@ -0,0 +1,76 @@
+package com.schuhr.Comm;
+
+import java.util.List;
+
+import org.ardulink.core.Link;
+import org.ardulink.core.convenience.Links;
+
+
+public class Arduino {
+
+	Link link;
+	
+	public Arduino(){
+		
+		//This is the connect stuff from version 1. Revert back if V2 doesn't work
+		/*try {*/
+/*			  Link link = Link.getDefaultInstance(); // 1
+			  // Comment this row if you use just the default connection
+			  link = getDigisparkConnection();
+
+			  List<String> portList = link.getPortList(); // 2
+			  if(portList != null && portList.size() > 0) {
+			    String port = portList.get(0);
+			    System.out.println("Connecting on port: " + port);
+			    boolean connected = link.connect(port); // 3
+			    System.out.println("Connected:" + connected);
+			    Thread.sleep(2000); // 4
+			    int power = IProtocol.HIGH;
+			    while(true) {
+			      System.out.println("Send power:" + power);
+			      link.sendPowerPinSwitch(2, power); // 5
+			      if(power == IProtocol.HIGH) {
+			        power = IProtocol.LOW;
+			      } else {
+			        power = IProtocol.HIGH;
+			      }
+			      Thread.sleep(2000);
+			    }
+			  } else {
+			    System.out.println("No port found!");
+			  }
+
+			} catch(Exception e) {
+			  e.printStackTrace();
+			}*/
+	}
+	
+	public void Connect(){
+		//Dont' know if this will actually work?
+		link = Links.getDefault(); //Returns the first serial port
+	}
+	
+	public void MoveForward(){
+		
+	}
+	
+	public void MoveBackward(){
+		
+	}
+	
+	public void TurnLeft(){
+		
+	}
+	
+	public void TurnRight(){
+		
+	}
+	
+	public void CenterWheels(){
+		
+	}
+	
+	public void Stop(){
+		
+	}
+}
---
+++ src/main/java/com/schuhr/Comm/Control.java
@@ -0,0 +1,110 @@
+package com.schuhr.Comm;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Thread;
+
+public class Control implements Runnable {
+
+	List<Character> keysPushed = new ArrayList<Character>();
+
+	private Boolean running = false;
+	private Thread t;
+	private String threadName = "";
+	private Arduino arduino;
+
+	public Control() {
+
+	}
+
+	public void run() {
+		try {
+
+			arduino = new Arduino();
+			arduino.Connect();
+
+			int port = 11000;
+
+			@SuppressWarnings("resource")
+			DatagramSocket dsocket = new DatagramSocket(port);
+			byte[] buffer = new byte[2048];
+
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+			while (running) {
+				dsocket.receive(packet);
+
+				String msg = new String(buffer, 0, packet.getLength());
+				System.out.println(packet.getAddress().getHostName() + ": " + msg);
+
+				//Process(msg, (msg.charAt(0) == '-') ? true : false);
+
+				// Reset the length of the packet before reusing it.
+				packet.setLength(buffer.length);
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+
+	public void Process(String message, Boolean remove) {
+		char cmd;
+		if (remove == true) {
+			cmd = message.charAt(1);
+			keysPushed.remove(cmd);
+		} else {
+			cmd = message.charAt(0);
+			keysPushed.add(cmd);
+		}
+
+		switch (cmd) {
+		case 'W':
+			if (remove == true) {
+				arduino.Stop();
+			} else {
+				if (!keysPushed.contains('S'))
+					arduino.MoveForward();
+			}
+			break;
+		case 'A':
+			if (remove == true) {
+				arduino.CenterWheels();
+			} else {
+				arduino.TurnLeft();
+			}
+			break;
+		case 'S':
+			if (remove == true) {
+				arduino.Stop();
+			} else {
+				if (!keysPushed.contains('W'))
+					arduino.MoveBackward();
+			}
+			break;
+		case 'D':
+			if (remove == true) {
+				arduino.TurnRight();
+			} else {
+				arduino.CenterWheels();
+			}
+			break;
+		}
+	}
+	
+	public Boolean Start(){
+		if (t == null) {
+			t = new Thread(this, threadName);
+			t.start();
+			running = true;
+			return true;
+		}
+		return false;
+	}
+	
+	public void Stop(){
+		running = false;
+	}
+
+}
